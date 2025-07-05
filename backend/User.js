3353688c2ecbcfb260d70c8c48bf7838340ea324
weed450 <bertudeelius3@gmail.com>
2025-07-05 11:15:10 +0300

    Update User.js

--- backend/User.js
+++ backend/User.js
@@ -1,1 +1,55 @@
 // User model
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  pseudo: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  email: {
+    type: String,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'modo', 'admin', 'owner', 'bot'],
+    default: 'user',
+  },
+  isVerified: {
+    type: Boolean,
+    default: false, // Badge bleu ✅
+  },
+  badges: {
+    type: [String], // ['ancien', 'supporter', 'top1', ...]
+    default: [],
+  },
+  country: String, // Pour la mini-carte interactive
+  xp: {
+    type: Number,
+    default: 0,
+  },
+  level: {
+    type: Number,
+    default: 1,
+  },
+  gems: {
+    type: Number,
+    default: 0,
+  },
+  inventory: {
+    type: [String], // liste d'objets (id ou nom)
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
