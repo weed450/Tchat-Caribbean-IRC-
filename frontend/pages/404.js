77ced4dfe2be912b93bb33bb7b739feea96436b4
weed450 <bertudeelius3@gmail.com>
2025-07-05 21:13:00 +0300

    Create 404.js

---
+++ frontend/pages/404.js
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-white">
+      <h1 className="text-4xl font-bold">404 - Page introuvable</h1>
+    </div>
+  );
+}
