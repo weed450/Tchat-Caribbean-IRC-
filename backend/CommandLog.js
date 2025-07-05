9dd1a35d1ad8d037607f7d7f7d020f04d2df73c0
weed450 <bertudeelius3@gmail.com>
2025-07-05 11:13:17 +0300

    Update CommandLog.js

--- backend/CommandLog.js
+++ backend/CommandLog.js
@@ -1,1 +1,27 @@
 // Command logs model
+// backend/models/CommandLog.js
+
+const mongoose = require('mongoose');
+
+const commandLogSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  command: {
+    type: String,
+    required: true,
+  },
+  parameters: {
+    type: String,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  room: {
+    type: String,
+  }
+});
+
+module.exports = mongoose.model('CommandLog', commandLogSchema);
