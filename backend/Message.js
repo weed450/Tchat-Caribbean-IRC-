0be2259aa9cf2c845c1074aed42d467dbe23b3b5
weed450 <bertudeelius3@gmail.com>
2025-07-05 11:14:09 +0300

    Update Message.js

--- backend/Message.js
+++ backend/Message.js
@@ -1,1 +1,30 @@
 // Message model
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  pseudo: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  room: {
+    type: String,
+    required: true,
+  },
+  isPrivate: {
+    type: Boolean,
+    default: false,
+  },
+  to: {
+    type: String, // utilisé pour les messages privés
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
