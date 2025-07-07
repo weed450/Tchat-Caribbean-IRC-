4ea33856902ef6378dd8872d6f7cbcee6afdf147
weed450 <bertudeelius3@gmail.com>
2025-07-07 14:19:21 -0500

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,1 +1,24 @@
-message.js
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  pseudo: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  room: {
+    type: String,
+    default: 'general',
+  },
+  badge: {
+    type: Object,
+    default: null,
+  },
+}, {
+  timestamps: true, // createdAt, updatedAt automatiques
+});
+
+module.exports = mongoose.model('Message', messageSchema);
